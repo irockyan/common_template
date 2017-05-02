c5c7b69e57bd19665400c24c27cd481d137c513d
Zyan <coderZyan@outlook.com>
2017-05-02 13:18:48 +0800

    修改配置:将css打包进js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path'),
-  ExtractTextPlugin = require('extract-text-webpack-plugin'),
   HtmlWebpackPlugin = require('html-webpack-plugin'),
   webpack = require('webpack');
 module.exports = {
@@ -14,11 +13,7 @@ module.exports = {
   module: {
     rules: [{
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'postcss-loader']
-        })
-
+        use: ['style-loader', 'css-loader', 'postcss-loader']
       }, {
         test: /\.js$/,
         use: 'babel-loader?sourceMap',
@@ -37,13 +32,14 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin({
-      filename: 'style.css',
-      allChunks: true
-    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './index.html')
     }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'commons',
+      filename: 'commons.js',
+      minChunks: 2
+    })
     // new webpack.optimize.UglifyJsPlugin()
   ],
   devtool: "cheap-eval-source-map",
